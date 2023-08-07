c3ee4455b735e62b6ced76f7cfbccad78b414cba
Marcelo Andres <mmendozaf@gmail.com>
2023-08-07 16:52:26 -0400

    feat: Prototype done

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  return:{
+    base: "turnos",
+  },
   build: {
     outDir: './docs',
-    return:{
-      site: "https://marceloandresmendoza.github.io",
-      base: "turnos",
-    }
   }
 })

dcbd95ed1e3cc62a97e4b9dc5106e11c27269be7
Marcelo Andres <mmendozaf@gmail.com>
2023-08-07 16:54:57 -0400

    feat: Prototype done

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  return:{
-    base: "turnos",
-  },
   build: {
     outDir: './docs',
+    base: "./turnos",
   }
 })
